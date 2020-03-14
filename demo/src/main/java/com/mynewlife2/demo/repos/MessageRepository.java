0d9b858db80688bae59f1bdec1f20c5946aab9ca
Khamit <khamitamantaev@gmail.com>
2020-03-15 03:31:58 +0500

    все готово почти!

---
+++ demo/src/main/java/com/mynewlife2/demo/repos/MessageRepository.java
@@ -0,0 +1,10 @@
+package com.mynewlife2.demo.repos;
+
+import com.mynewlife2.demo.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
+}
