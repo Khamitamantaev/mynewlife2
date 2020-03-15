4171a82195804b12c753db42d23141d64214f727
Khamit <khamitamantaev@gmail.com>
2020-03-15 18:57:18 +0500

    все отлично работает

--- demo/src/main/java/com/mynewlife2/demo/controllers/MainController.java
+++ demo/src/main/java/com/mynewlife2/demo/controllers/MainController.java
@@ -27,9 +27,16 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String,Object> model) {
+    public String main(@RequestParam(required = false , defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages" , messages);
+        if(filter != null && !filter.isEmpty()){
+            messages = messageRepository.findByTag(filter);
+        }
+        else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages" , messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
     @PostMapping("/main")
@@ -42,19 +49,6 @@ public class MainController {
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages" , messages);
-        return "main";
-    }
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter, Map<String,Object> model) {
-        Iterable<Message> messages;
-        if(filter != null && !filter.isEmpty()){
-            messages = messageRepository.findByTag(filter);
-        }
-        else {
-            messages = messageRepository.findAll();
-        }
-       model.put("messages", messages);
-
         return "main";
     }
 }
